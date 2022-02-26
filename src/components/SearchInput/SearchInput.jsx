e8d4be5828517c0f3bb758f2e2244dc0a909154e
unknown <ahmed.3laa.2020@gmail.com>
2022-02-26 18:40:19 +0200

    Implement restaurant details page

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -3,7 +3,7 @@ import "./SearchInput.css"
 const SearchInput = ({onSearch}) => {
   return (
     <div className="search-input">
-      <svg className="svg-icon search-icon form-control-feedback" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91"><path strokeLinecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
+      <svg className="svg-icon search-icon form-control-feedback" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g className="search-path" fill="none" stroke="#848F91"><path strokeLinecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
       <input type="text" className="form-control" placeholder="Search" onChange={onSearch} />
     </div>
   )
--- src/views/RestaurantDetails/RestaurantDetails.jsx
+++ src/views/RestaurantDetails/RestaurantDetails.jsx
@@ -1,10 +1,83 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import "./RestaurantDetails.css";
 
 const RestaurantDetails = () => {
+  const restaurantsStore = useSelector((state) => state.restaurantsReducer);
+  const [restaurantData, setRestaurantData] = useState(null);
+  const { name } = useParams();
+  const renderRestaurant = () => {
+    return (
+      restaurantsStore.restaurants.length &&
+      restaurantsStore.restaurants.map(
+        (restaurant) =>
+          restaurant.name === name && setRestaurantData(restaurant)
+      )
+    );
+  };
+
+  useEffect(() => {
+    renderRestaurant();
+  }, []);
+
   return (
     <div className="container">
-      <h2 className="text-center text-success">RestaurantDetails</h2>
-      <Link to="/">Back to home</Link>
+      {restaurantData ? (
+        <div className="card restaurant-details">
+          <div className="logo">
+            <img
+              className="card-img-top "
+              src={restaurantData.logo}
+              alt={restaurantData.name}
+            />
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{restaurantData.description}</h5>
+            <hr />
+            {/* items  */}
+            {restaurantData.items.length ? (
+              <>
+                <h3>Items</h3>
+                <div className="items-list">
+                  {restaurantData.items.map((item,i) => {
+                    return (
+                      <div key={i} className="card text-white bg-dark mb-3 item">
+                        <div className="card-header">{item.name}</div>
+                        <div className="card-body">
+                          <h5 className="card-title">{item.price}</h5>
+                          <p className="card-text">{item.description}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <hr />
+              </>
+            ) : (
+              <p>No items for {restaurantData.name}</p>
+            )}
+            {/* branches  */}
+            {restaurantData.branches.length ? (
+              <>
+                <h3>Branches</h3>
+                <div className="branches-list">
+                  {restaurantData.branches.map((branch,i) => {
+                    return <p key={i}>{branch.name}</p>;
+                  })}
+                </div>
+              </>
+            ) : (
+              <p>No branch for {restaurantData.name}</p>
+            )}
+          </div>
+        </div>
+      ) : (
+        (<h2 className="text-center py-5">Press back to refresh data</h2>)
+      )}
+      <Link className="btn btn-outline-dark d-grid my-4" to="/">
+        Back to home
+      </Link>
     </div>
   );
 };
